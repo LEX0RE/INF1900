d43fc2f003af463f3b4f29831d33c7f2e673edcd
Alexandre Gélinas <60028306+LEX0RE@users.noreply.github.com>
2021-03-05 07:59:57 -0500

    Update to TP6

--- tp/tp4/pb3/pb3.cpp
+++ tp/tp4/pb3/pb3.cpp
@@ -22,11 +22,13 @@ void ajustementPWM ( unsigned char signal) {
 }
 
 void initialisation(){
+  DDRB = 0xff;
+  DDRD = 0xff;
   TCNT1 = 0;
   PORTD = 0;
 
   // division d'horloge par 8 - implique une frequence de PWM fixe
-  TCCR1A |= (1 << COM1B1) | (1 << COM1B0) | (1 << COM1A1) | (1 << COM1A0); // Output sur OC1A et OC1B
+  TCCR1A |= (1 << COM1B1) | (1 << COM1B0) | (1 << COM1A1) | (1 << COM1A0); // Output sur OC1A et OC1B (Set Up, Clear Down)
   TCCR1A |= (1 << WGM10); // Mode 1
   TCCR1A &= ~(1 << WGM11); // Mode 1
   TCCR1B &= ~(1 << WGM12) & ~(1 << WGM13); // Mode 1
@@ -36,41 +38,15 @@ void initialisation(){
   ajustementPWM(254);
 }
 
-void ajustementPWM (int signal) {
-
-// mise à un des sorties OC1A et OC1B sur comparaison
-// réussie en mode PWM 8 bits, phase correcte
-// et valeur de TOP fixe à 0xFF (mode #1 de la table 16-5
-// page 130 de la description technique du ATmega324PA)
-
-  OCR1A = int((255 * signal) / 100);
-  OCR1B = int((255 * signal) / 100);
-
-// division d'horloge par 8 - implique une frequence de PWM fixe
-
-  TCCR1A |= 1 << WGM10;
-  TCCR1A &= 1 << WGM11;
-  TCCR1B &= ~(1 << WGM12) & ~(1 << WGM13) & ~(1 << CS10) & ~(1 << CS12);
-  TCCR1B |= 1 << CS11;
-  TCCR1C = 0;
-
-}
-
 int main(){
-  DDRB = 0xff;
-  DDRD = 0xff;
   bool direction = 1;
-  int pourcentage = 0;
   initialisation();
+  PORTB = direction;
 
-  for(;;)  // boucle sans fin
+  for(unsigned pourcentage = 25; pourcentage <= 100; pourcentage += 25)
   {
-    PORTB = direction;
     _delay_ms(2000);
-    if (pourcentage < 100){
-      pourcentage += 25;
-      ajustementPWM(char(255 - (pourcentage * 255 / 100)));
-    }
+    ajustementPWM(255 - (pourcentage * 255 / 100));
   }
   return 0;
 }
--- tp/tp5/pb1/pb1.cpp
+++ tp/tp5/pb1/pb1.cpp
@@ -11,19 +11,31 @@
 #include <avr/io.h>
 #include <avr/eeprom.h>
 #include <util/delay.h>
-
+#include <string.h>
 //fonction à faire
 
 int main(){
   DDRB = 0xff;
-  PINB = 0b0;
+  PORTB = 0b0;
 
   uint8_t* adresse = 0x00;
-  uint8_t MessageRecu;
-  uint8_t MessageEnvoye = '*';
-  eeprom_write_byte(adresse, MessageEnvoye);
-  MessageRecu = eeprom_read_byte(adresse);
+  char MessageRecu [60] = {};
+  char MessageEnvoye [] = "*P*O*L*Y*T*E*C*H*N*I*Q*U*E* *M*O*N*T*R*E*A*L*";
+
+  for (unsigned i = 0 ; MessageEnvoye[i] != '\0' ; i++) 
+    eeprom_write_byte(adresse++, MessageEnvoye[i]);
+
+  //eeprom_update_block((const void*) &MessageEnvoye, (void*) 0, sizeof(MessageEnvoye));
+  //eeprom_read_block((void*) & MessageRecu, (const void*) 0, sizeof(MessageEnvoye));
+     
+  adresse = 0;  
+  for (unsigned i = 0 ; MessageEnvoye[i] != '\0' ; i++)
+    MessageRecu[i] = eeprom_read_byte(adresse++);
   
-  if (MessageEnvoye == MessageRecu)
-    PINB = 0b1;  return 0;
+  if ((strcmp((char*) MessageRecu, (char*) MessageEnvoye)) == 0)
+    PORTB = 0b01;  
+  else
+    PORTB = 0b10;
+  
+  return 0;
 }
